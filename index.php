68fb15cf0c8994457e48cebfd7f5146aad3c1228
refojunior <refojunior5@gmail.com>
2018-01-16 15:48:28 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>	Refo Junior - Website Developer</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.css">
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Mukta+Vaani" rel="stylesheet">
+	<link rel="icon" href="img/icon.jpg">
+</head>
+<body>
+<div class="container-fluid">
+	<div class="row">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+		  <a class="navbar-brand" href="#">
+		    <img src="img/logo.jpg" height="50" alt="">
+		  </a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarNav">
+		    <ul class="navbar-nav mr-auto">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Portfolio</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Blog</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Contact</a>
+		      </li>
+		    </ul>
+
+		    <form class="form-inline my-2 my-lg-0">
+			    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			    <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+			</form>
+		  </div>
+		  
+		</nav>
+	</div>
+
+	<div class="row fit">
+		<img src="img/carousel.jpg" class="bg" width="100%" alt="img-home">
+		<div class="box-home box-home-t box-home-m">
+			<h1 align="center">Welcome to My Site</h1>
+			<hr>
+			<p class="text-home">Hi, my name is Refo Junior Farigia, a Website Developer based in Denpasar, Bali. I'm a student in STIMIK STIKOM Bali and i really love to make a website. 
+			So contact me now to get special price!</p>
+			<a href="#" class="btn btn-outline-primary"><i class="fa fa-envelope"></i> Contact Me</a>
+		</div>
+	</div>
+	<br><br>
+	
+	<div class="row justify-content-md-center">
+		<h1>Simple About Me</h1>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-sm-6 col-lg-6">
+				<img src="img/me.jpg" class="img-thumbnail" alt="">
+			</div>
+			<div class="col-md-6 col-sm-6 col-lg-6">
+				wkwkw
+			</div>
+		</div>
+	</div>
+	<br><br>
+
+	<div class="row justify-content-md-center">
+		<h1>My Skills</h1>
+	</div>
+	<div class="row relative">
+		<img src="img/skills.jpg" class="bg" width="100%" height="500px" alt="">
+		<div class="content-skills">
+			<h2>HTML</h2>
+		</div>
+		
+	</div>
+	<br><br>
+
+
+	<div class="row justify-content-md-center">
+		<h1>My Porfolio</h1>
+	</div>
+	<div class="container">
+		<div class="row">	
+			<div class="col-md-4 col-sm-4 col-lg-4">
+				<img src="img/1.jpg" class="img-thumbnail" alt="">
+			</div>
+			<div class="col-md-4 col-sm-4 col-lg-4">
+				<img src="img/2.jpg" class="img-thumbnail" alt="">
+			</div>
+			<div class="col-md-4 col-sm-4 col-lg-4">
+				<img src="img/3.jpg" class="img-thumbnail" alt="">
+			</div>
+		</div>
+		<br>
+		<div class="row">	
+			<div class="col-md-4 col-sm-4 col-lg-4">
+				<img src="img/1.jpg" class="img-thumbnail" alt="">
+			</div>
+			<div class="col-md-4 col-sm-4 col-lg-4">
+				<img src="img/2.jpg" class="img-thumbnail" alt="">
+			</div>
+			<div class="col-md-4 col-sm-4 col-lg-4">
+				<img src="img/3.jpg" class="img-thumbnail" alt="">
+			</div>
+		</div>
+	</div>
+	
+
+	
+	
+
+
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+</body>
+</html>
