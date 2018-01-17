3fff4cfee68bf90865f6700323432d202e017c14
refojunior <refojunior5@gmail.com>
2018-01-17 22:53:27 +0800

    commit malam di wc

--- index.php
+++ index.php
@@ -211,72 +211,82 @@
 	<div class="row">
 		<div class="container-fluid">
 		    <div class="row" id="contact">
-				<img src="img/contact2.jpg" class="img-contact" alt="bg-img">
+				<img src="img/contact-.jpg" class="img-contact" alt="bg-img">
 				<div class="col-lg-12 col-md-12 col-xs-12 absolute" >
 				      <br>
 				      <div class="row justify-content-center">
 				      	<h1 style="color:black; background:white">Contact Me</h1> 
 				      </div>
-		
-					<div class="row">
-						<div class="col-lg-6 col-md-6 col-xs-6">
-							<div class="address">
-		                        <div class="address-grid">
-		                            <div class="address-left">
-		                                <span class="fa fa-phone" aria-hidden="true"></span>
-		                            </div>
-		                            <div class="address-right">
-		                                <h6>Phone Number</h6>
-		                                <p>+6283114738768</p>
-		                            </div>
-		                            <br><br>
-		                        </div>
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-6 col-md-6 ">
+								
+			                        <div class="address-grid">
+			                            <div class="address-left">
+			                                <span class="fa fa-phone" aria-hidden="true"></span>
+			                            </div>
+			                            <div class="address-right">
+			                                <h6>Phone Number</h6>
+			                                <p>+6283114738768</p>
+			                            </div>
+			                            <br><br>
+			                       
+								</div>
+								<div class="address">
+			                        <div class="address-grid">
+			                            <div class="address-left">
+			                                <span class="fa fa-envelope" aria-hidden="true"></span>
+			                            </div>
+			                            <div class="address-right">
+			                                <h6>Email</h6>
+			                                <p>me@refojunior.com</p>
+			                            </div>
+			                            <br><br>
+			                        </div>
+								</div>
+								<div class="address">
+			                        <div class="address-grid">
+			                            <div class="address-left">
+			                                <span class="fa fa-map-marker" aria-hidden="true"></span>
+			                            </div>
+			                            <div class="address-right">
+			                                <h6>Location</h6>
+			                                <p>Jl. Gn. Andakasa GG. Cempaka 1/9 Denpasar, Bali</p>
+			                            </div>
+			                            <br><br><br>
+			                        </div>
+								</div>
+								<br>
+								<div class="socmed">
+									<h3>My Social Media</h3>
+									<div class="fb"><a href="#"><i class="fa fa-facebook-official"></i></a></div>
+									<div class="twitter"><a href="#"><i class="fa fa-twitter"></i></a></div>
+									<div class="ig"><a href="#"><i class="fa fa-instagram"></i></a></div>
+									<div class="yt"><a href="#"><i class="fa fa-youtube"></i></a></div>
+									
+								</div>
 							</div>
-							<div class="address">
-		                        <div class="address-grid">
-		                            <div class="address-left">
-		                                <span class="fa fa-envelope" aria-hidden="true"></span>
-		                            </div>
-		                            <div class="address-right">
-		                                <h6>Email</h6>
-		                                <p>me@refojunior.com</p>
-		                            </div>
-		                            <br><br>
-		                        </div>
-							</div>
-							<div class="address">
-		                        <div class="address-grid">
-		                            <div class="address-left">
-		                                <span class="fa fa-map-marker" aria-hidden="true"></span>
-		                            </div>
-		                            <div class="address-right">
-		                                <h6>Location</h6>
-		                                <p>Jl. Gn. Andakasa GG. Cempaka 1/9 Denpasar, Bali</p>
-		                            </div>
-		                            <br><br><br>
-		                        </div>
+							
+							<div class="col-lg-6 col-md-6 col-xs-6 bg-contact">
+								<div class="contact-form">
+									<br>
+									<h3 style="background: white; width: 240px">Hubungi Sekarang : </h3>
+									<b>Nama Lengkap :</b>
+									<input type="text" class="form-control" placeholder="Nama Lengkap">
+									<b>Email :</b>
+									<input type="text" class="form-control" placeholder="Email">
+									<b>Subject :</b>  
+									<input type="text" class="form-control" placeholder="Subject">
+									<b>Pesan :</b>  
+									<textarea class="form-control" placeholder="Pesan"></textarea>
+									<br>
+									<input type="submit" class="btn btn-lg btn-primary" value="Kirim">
+									<br>
+									<br>
+								</div>
 							</div>
 						</div>
-						
-						<div class="col-lg-6 col-md-6 col-xs-6 bg-contact">
-							<div class="contact-form">
-								<br>
-								<h2 style="font-weight:bold; color:white">Hubungi Sekarang : </h2>
-								Nama Lengkap : 
-								<input type="text" class="form-control">
-								Email : 
-								<input type="text" class="form-control">
-								Subject : 
-								<input type="text" class="form-control">
-								Pesan : 
-								<textarea class="form-control"></textarea>
-								<br>
-								<input type="submit" class="btn btn-lg btn-primary" value="Kirim">
-								<br>
-								<br>
-							</div>
-						</div>
-					</div>
+					</div>	
 					<br>
 					<div class="row">
 						<div class="footer">
